895f06323a8737627d5af068dd155e115380702c
Lachie-Source <62362511+Lachie-Source@users.noreply.github.com>
2021-03-21 17:36:54 +1100

    why is this here

--- src/tokens.ts
+++ src/tokens.ts
@@ -12,11 +12,3 @@ export var TokenRegex = {
   NUMBER: /[0-9]/,
   ALPHABET: /[A-z]/,
 };
-
-function abstractSyntaxTree(input: string): AbstractSyntaxTree {
-  throw new Error("Function not implemented");
-}
-
-function generateCode(tokens: AbstractSyntaxTree): string {
-  throw new Error("Function not implemented");
-}
